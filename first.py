f2fa6cab39246b3d1896afa3d2e440a183bdc660
AlexanderTejerizo <alexat2007@outlook.com>
2025-06-09 21:28:32 -0400

    First commit

---
+++ first.py
@@ -0,0 +1,29 @@
+# # Lesson 1: Variables 
+# fishFace = 1 camelCase
+# print(fishFace)
+
+# this_is_snake_case= 2 Snake case
+# print(this_is_snake_case)
+
+# re4l = 3 Number Variable
+# print(re4l)
+
+#Data points
+Float_1 = 1.5
+                #Floating Points
+Float_2 = 2.5
+
+Int_1 = 1
+                #Integer
+Int_2 = 2
+
+Boolean_1 = True
+                #Boolean
+Boolean_2 = False 
+
+print(Float_1)
+print(Float_2)
+print(Int_1)
+print(Int_2)
+print(Boolean_1)
+print(Boolean_2)
