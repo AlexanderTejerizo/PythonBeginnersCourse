fd28843674c86a5322be76e789c0d5b75fbc0495
AlexanderTejerizo <alexat2007@outlook.com>
2025-06-10 23:31:37 -0400

    Your commit message

--- first.py
+++ first.py
@@ -8,22 +8,50 @@
 # re4l = 3 Number Variable
 # print(re4l)
 
-#Data points
-Float_1 = 1.5
-                #Floating Points
-Float_2 = 2.5
+# #Data points
+# Float_1 = 1.5
+#                 #Floating Points
+# Float_2 = 2.5
 
-Int_1 = 1
-                #Integer
-Int_2 = 2
+# Int_1 = 1
+#                 #Integer
+# Int_2 = 2
 
-Boolean_1 = True
-                #Boolean
-Boolean_2 = False 
+# Boolean_1 = True
+#                 #Boolean
+# Boolean_2 = False 
 
-print(Float_1)
-print(Float_2)
-print(Int_1)
-print(Int_2)
-print(Boolean_1)
-print(Boolean_2)
+# print(Float_1)
+# print(Float_2)
+# print(Int_1)
+# print(Int_2)
+# print(Boolean_1)
+# print(Boolean_2)
+
+# # Number Operators
+# Addition = 1 + 1
+# Subtraction = 3 - 2
+# Multiplication = 2 * 2
+# Division = 10 / 2
+
+# Exponentiation = 4 ** 4
+# floorDevision = 10 // 3
+# Modulo = 10 % 3
+
+# # Assignments
+# Addition += 5
+# Subtraction -= 3
+# Division /= 2.5
+
+# print(Division)
+
+# Grocery Store Programming Challenge
+penne = 16.88
+Pasta_Sauce = 6.98
+olives = 16.76
+I_seasoning = 15.26
+baguettes = 3.00
+meatballs = 4.39
+
+subtotal = round(penne + Pasta_Sauce + olives + I_seasoning + baguettes + meatballs, 2)
+print(subtotal)
